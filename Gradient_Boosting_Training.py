89f5316e26be3fdd597fd6a185c6fcfbbffdb9d8
Atychi762 <143887269+Atychi762@users.noreply.github.com>
2025-11-12 17:31:18 +0000

    Adding scaling to the feature columns to ensure an intepretable target

--- Gradient_Boosting_Training.py
+++ Gradient_Boosting_Training.py
@@ -1,12 +1,19 @@
 import pandas as pd
+import numpy as np
+
 from sklearn.ensemble import GradientBoostingRegressor as gbr
 from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
 
 dataset = pd.read_csv('datasets/steel.csv')
 
+scaler = StandardScaler()
+feature_cols = dataset.select_dtypes(include=[np.number]).columns.drop('tensile_strength')
+dataset[feature_cols] = scaler.fit_transform(dataset[feature_cols])
+
 kf = KFold(n_splits=10, shuffle=False)
-average_error = 0
+average_mean_squared_error = 0
 
 for train, test in kf.split(dataset):
     train_data = dataset.iloc[train]
@@ -24,7 +31,7 @@ for train, test in kf.split(dataset):
     mse = mean_squared_error(y_test, predictions)
     print(f'Mean Squared Error: {mse}')
 
-    average_error += mse
+    average_mean_squared_error += mse
 
-average_error /= 10
-print(f'Average Mean Squared Error over 10 folds: {average_error}')
+average_mean_squared_error /= 10
+print(f'\nAverage Mean Squared Error over 10 folds: {average_mean_squared_error}')
