6df4f59d7906e8d588c3066cd552e37a24565413
Atychi762 <143887269+Atychi762@users.noreply.github.com>
2025-11-12 19:06:00 +0000

    Adding predicitons on training data, added calculations for basic metrics

--- Gradient_Boosting_Training.py
+++ Gradient_Boosting_Training.py
@@ -15,8 +15,10 @@ feature_cols = dataset.columns.drop('tensile_strength')
 dataset[feature_cols] = scaler.fit_transform(dataset[feature_cols])
 
 kf = KFold(n_splits=10, shuffle=False)
-default_average_root_mean_squared_error = 0
-tuned_average_root_mean_squared_error = 0
+default_average_rmse_test = 0
+tuned_average_rmse_test = 0
+default_average_rmse_train = 0
+tuned_average_rmse_train = 0
 
 rmse_output = []
 
@@ -35,8 +37,13 @@ for train, test in kf.split(dataset):
     default_model = GradientBoostingRegressor()
     default_model.fit(X_train, y_train)
 
-    default_predictions = default_model.predict(X_test)
-    default_rmse = root_mean_squared_error(y_test, default_predictions)
+    # Calculating RMSE on training data
+    default_train_predictions = default_model.predict(X_train)
+    default_train_rmse = root_mean_squared_error(y_train, default_train_predictions)
+    
+    # Calculating RMSE on test data
+    default_test_predictions = default_model.predict(X_test)
+    default_test_rmse = root_mean_squared_error(y_test, default_test_predictions)
 
     # Training the tuned GBR model
     # Varied hyperparameters are learning rate and n_estimators
@@ -58,22 +65,35 @@ for train, test in kf.split(dataset):
 
     best_model = grid_search.best_estimator_
     # Using the best hyperparameters found
-    tuned_predictions = best_model.predict(X_test)
-    tuned_rmse = root_mean_squared_error(y_test, tuned_predictions)
+    # Calculating RMSE on training data
+    tuned_train_predictions = best_model.predict(X_train)
+    tuned_train_rmse = root_mean_squared_error(y_train, tuned_train_predictions)
+
+    # Calculating RMSE on test data
+    tuned_test_predictions = best_model.predict(X_test)
+    tuned_test_rmse = root_mean_squared_error(y_test, tuned_test_predictions)
 
     # Appending RMSE for the current fold
-    rmse_output.append({"Default_RMSE": default_rmse,
-                       "Tuned_RMSE": tuned_rmse})
+    rmse_output.append({"Default_TrainData_RMSE": default_train_rmse,
+                        "Default_TestData_RMSE": default_test_rmse,
+                       "Tuned_TrainData_RMSE": tuned_train_rmse,
+                       "Tuned_TestData_RMSE": tuned_test_rmse})
 
-    default_average_root_mean_squared_error += default_rmse
-    tuned_average_root_mean_squared_error += tuned_rmse
+    default_average_rmse_test += default_test_rmse
+    tuned_average_rmse_test += tuned_test_rmse
+    default_average_rmse_train += default_train_rmse
+    tuned_average_rmse_train += tuned_train_rmse
 
 # Appending average RMSE to the results
-default_average_root_mean_squared_error /= 10
-tuned_average_root_mean_squared_error /= 10
+default_average_rmse_test /= 10
+tuned_average_rmse_test /= 10
+default_average_rmse_train /= 10
+tuned_average_rmse_train /= 10
 
-rmse_output.append({"Default_RMSE": default_average_root_mean_squared_error,
-                   "Tuned_RMSE": tuned_average_root_mean_squared_error})
+rmse_output.append({ "Default_TrainData_RMSE": default_average_rmse_train,
+                   "Default_TestData_RMSE": default_average_rmse_test,
+                   "Tuned_TrainData_RMSE": tuned_average_rmse_train,
+                   "Tuned_TestData_RMSE": tuned_average_rmse_test})
 
 # Saving RMSE results to a CSV file
 rmse_df = pd.DataFrame(rmse_output)
