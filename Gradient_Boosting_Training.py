6741c6b184cf3255d815f08974116146cc7054e5
Atychi762 <143887269+Atychi762@users.noreply.github.com>
2025-11-12 17:23:32 +0000

    Cresting the basic K-fold training of gradient boosting with mean squared error

--- Gradient_Boosting_Training.py
+++ Gradient_Boosting_Training.py
@@ -1,5 +1,30 @@
 import pandas as pd
 from sklearn.ensemble import GradientBoostingRegressor as gbr
 from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error
 
 dataset = pd.read_csv('datasets/steel.csv')
+
+kf = KFold(n_splits=10, shuffle=False)
+average_error = 0
+
+for train, test in kf.split(dataset):
+    train_data = dataset.iloc[train]
+    test_data = dataset.iloc[test]
+
+    X_train = train_data.drop('tensile_strength', axis=1)
+    y_train = train_data['tensile_strength']
+    X_test = test_data.drop('tensile_strength', axis=1)
+    y_test = test_data['tensile_strength']
+
+    model = gbr()
+    model.fit(X_train, y_train)
+
+    predictions = model.predict(X_test)
+    mse = mean_squared_error(y_test, predictions)
+    print(f'Mean Squared Error: {mse}')
+
+    average_error += mse
+
+average_error /= 10
+print(f'Average Mean Squared Error over 10 folds: {average_error}')
